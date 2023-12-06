c614d0ce8d5bb00109448ca68b5e6a26f6a4f094
Tobias-soderberg <145038029+Tobias-soderberg@users.noreply.github.com>
2023-12-06 16:30:28 +0100

    Image fixed

--- Windows/MyGardenWindow.xaml.cs
+++ Windows/MyGardenWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using GreenThumb.Database;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace GreenThumb.Windows
@@ -12,25 +14,6 @@ namespace GreenThumb.Windows
         public MyGardenWindow()
         {
             InitializeComponent();
-
-
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Image img = new Image { };
-                    BitmapImage bitmapImage = new BitmapImage(new Uri(@"https://media.discordapp.net/attachments/1181964626632638598/1181964689824030850/Flower2.jpg?ex=6582f965&is=65708465&hm=5f233a079b12525ad8405bdd13f8b0ea865c24eba89968b356dc2666eb76bae3&=&format=webp&width=909&height=909"));
-                    img.Source = bitmapImage;
-
-                    img.Stretch = System.Windows.Media.Stretch.UniformToFill;
-
-                    img.SetValue(Grid.ColumnProperty, i);
-                    img.SetValue(Grid.RowProperty, j);
-
-                    gridGarden.Children.Add(img);
-                }
-
-            }
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -38,6 +21,51 @@ namespace GreenThumb.Windows
             PlantWindow plantWindow = new PlantWindow();
             plantWindow.Show();
             Close();
+        }
+
+        private void DisplayPlants()
+        {
+            using (GreenThumbDbContext context = new())
+            {
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //Create grid that is for each cell
+                    Grid cellGrid = new Grid();
+                    cellGrid.SetValue(Grid.ColumnProperty, i);
+                    cellGrid.SetValue(Grid.RowProperty, j);
+
+                    //Set image source and stretch it out to fill cell
+                    Image img = new Image { };
+                    BitmapImage bitmapImage = new BitmapImage(new Uri(@"https://media.discordapp.net/attachments/1181964626632638598/1181964689824030850/Flower2.jpg?ex=6582f965&is=65708465&hm=5f233a079b12525ad8405bdd13f8b0ea865c24eba89968b356dc2666eb76bae3&=&format=webp&width=909&height=909"));
+                    img.Source = bitmapImage;
+
+                    //Set Values on image
+                    img.Stretch = System.Windows.Media.Stretch.UniformToFill;
+
+                    //Create textblock
+                    TextBlock numberTextBlock = new TextBlock
+                    {
+                        Text = 1.ToString(), // Change 1 to Quantity of the flower!
+                        Foreground = Brushes.Black,
+                        Background = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Bottom
+                    };
+
+                    //Add to the cell grid
+                    cellGrid.Children.Add(img);
+                    cellGrid.Children.Add(numberTextBlock);
+
+                    //Add cellgrid to main one
+                    gridGarden.Children.Add(cellGrid);
+                }
+
+            }
         }
     }
 }
